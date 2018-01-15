92fc13f80d8931bef9026b93fce73598d7a1211a
IMakeGames <arbolperro@gmail.com>
2018-01-14 22:48:43 -0300

    - Improved the creation of Map Tiles and fixed the logic which was problematic when assigning x and y possitions

--- src/obj/map/main_map.rb
+++ src/obj/map/main_map.rb
@@ -1,180 +1,183 @@
 require '../../src/obj/map/map_tile'
 class MainMap
+  TILE_WIDTH = 12
+  TILE_HEIGHT = 12
 
   def initialize
-      @map_sprites = Gosu::Image.load_tiles("../../assets/sprites/Mapas/forest_floor_tiles.png", 12, 12, retro: true)
-      @plain_d            = MapTile.new(@map_sprites[0])
-      @flowers            = MapTile.new(@map_sprites[1])
-      @stone              = MapTile.new(@map_sprites[2])
-      @plain              = MapTile.new(@map_sprites[3])
-      @hwall1              = MapTile.new(@map_sprites[4])
-      @hwall2              = MapTile.new(@map_sprites[5])
-      @walldt             = MapTile.new(@map_sprites[6])
-      @walldb             = MapTile.new(@map_sprites[7])
-      @vwall1             = MapTile.new(@map_sprites[8])
-      @vwall2             = MapTile.new(@map_sprites[9])
-      @BRwallc             = MapTile.new(@map_sprites[10])
-      @vwallr1             = MapTile.new(@map_sprites[11])
-      @vwallr2             = MapTile.new(@map_sprites[12])
-      @plain_s             = MapTile.new(@map_sprites[13])
-      @walldtr             = MapTile.new(@map_sprites[14])
-      @walldbr             = MapTile.new(@map_sprites[15])
-      @dirt1             = MapTile.new(@map_sprites[16])
-      @dirt2             = MapTile.new(@map_sprites[17])
-      @dirtB1             = MapTile.new(@map_sprites[18])
-      @dirtB2             = MapTile.new(@map_sprites[19])
-      @dirtT1             = MapTile.new(@map_sprites[20])
-      @dirtT2             = MapTile.new(@map_sprites[21])
-      @BRdirtc             = MapTile.new(@map_sprites[22])
-      @LBdirtc           = MapTile.new(@map_sprites[23])
-      @TRdirtc             = MapTile.new(@map_sprites[24])
-      @LTdirtc           = MapTile.new(@map_sprites[25])
-      @dirtR1             = MapTile.new(@map_sprites[26])
-      @dirtR2             = MapTile.new(@map_sprites[27])
-      @dirtL1             = MapTile.new(@map_sprites[28])
-      @dirtL2             = MapTile.new(@map_sprites[29])
-      @LBwallc             = MapTile.new(@map_sprites[30])
-      @LBwallc             = MapTile.new(@map_sprites[30])
-      @Thedge1              = MapTile.new (@map_sprites[31])
-      @Thedge2              = MapTile.new (@map_sprites[32])
-      @Lhedge1              = MapTile.new (@map_sprites[33])
-      @Lhedge2              = MapTile.new (@map_sprites[34])
-      @Rhedge1              = MapTile.new (@map_sprites[35])
-      @Rhedge2              = MapTile.new (@map_sprites[36])
-      @Bhedge1              = MapTile.new (@map_sprites[37])
-      @Bhedge2              = MapTile.new (@map_sprites[38])
-      @BRhedgec             = MapTile.new (@map_sprites[39])
-      @LBhedgec             = MapTile.new (@map_sprites[40])
-      @TRhedgec             = MapTile.new (@map_sprites[41])
-      @LThedgec             = MapTile.new (@map_sprites[42])
-      @underbrush1             = MapTile.new (@map_sprites[43])
-      @underbrush2             = MapTile.new (@map_sprites[44])
+    @map_sprites = Gosu::Image.load_tiles("../../assets/sprites/Mapas/forest_floor_tiles.png", TILE_WIDTH, TILE_HEIGHT, retro: true)
+    @plain_d = @map_sprites[0]
+    @flowers = @map_sprites[1]
+    @stone = @map_sprites[2]
+    @plain = @map_sprites[3]
+    @hwall1 = @map_sprites[4]
+    @hwall2 = @map_sprites[5]
+    @walldt = @map_sprites[6]
+    @walldb = @map_sprites[7]
+    @vwall1 = @map_sprites[8]
+    @vwall2 = @map_sprites[9]
+    @BRwallc = @map_sprites[10]
+    @vwallr1 = @map_sprites[11]
+    @vwallr2 = @map_sprites[12]
+    @plain_s = @map_sprites[13]
+    @walldtr = @map_sprites[14]
+    @walldbr = @map_sprites[15]
+    @dirt1 = @map_sprites[16]
+    @dirt2 = @map_sprites[17]
+    @dirtB1 = @map_sprites[18]
+    @dirtB2 = @map_sprites[19]
+    @dirtT1 = @map_sprites[20]
+    @dirtT2 = @map_sprites[21]
+    @BRdirtc = @map_sprites[22]
+    @LBdirtc = @map_sprites[23]
+    @TRdirtc = @map_sprites[24]
+    @LTdirtc = @map_sprites[25]
+    @dirtR1 = @map_sprites[26]
+    @dirtR2 = @map_sprites[27]
+    @dirtL1 = @map_sprites[28]
+    @dirtL2 = @map_sprites[29]
+    @LBwallc = @map_sprites[30]
+    @LBwallc = @map_sprites[30]
+    @Thedge1 = @map_sprites[31]
+    @Thedge2 = @map_sprites[32]
+    @Lhedge1 = @map_sprites[33]
+    @Lhedge2 = @map_sprites[34]
+    @Rhedge1 = @map_sprites[35]
+    @Rhedge2 = @map_sprites[36]
+    @Bhedge1 = @map_sprites[37]
+    @Bhedge2 = @map_sprites[38]
+    @BRhedgec = @map_sprites[39]
+    @LBhedgec = @map_sprites[40]
+    @TRhedgec = @map_sprites[41]
+    @LThedgec = @map_sprites[42]
+    @underbrush1 = @map_sprites[43]
+    @underbrush2 = @map_sprites[44]
 
-      @tree1             = MapTile.new (@map_sprites[48])
-      @tree2             = MapTile.new (@map_sprites[49])
-      @tree3             = MapTile.new (@map_sprites[56])
-      @tree4             = MapTile.new (@map_sprites[57])
+    @tree1 = @map_sprites[48]
+    @tree2 = @map_sprites[49]
+    @tree3 = @map_sprites[56]
+    @tree4 = @map_sprites[57]
 
 
+    @map_tiles = []
 
-      @map_tiles = []
-
-      f = File.open("../../src/obj/map/map1_layout.txt")
-      f.each do |line|
-        @line = []
-        line.split(",").each do |n|
-          case n
-            when '1'
-              @line << @plain
-            when '2'
-              @line << @plain_d
-            when '3'
-              @line << @plain_s
-            when '4'
-              @line << @stone
-            when '5'
-              @line << @flowers
-            when '6'
-              @line << @dirt1
-            when '7'
-              @line << @dirt2
-            when '8'
-              @line << @BRdirtc
-            when '9'
-              @line << @LBdirtc
-            when 'a'
-              @line << @TRdirtc
-            when 'b'
-              @line << @LTdirtc
-            when 'c'
-              @line << @dirtB1
-            when 'd'
-              @line << @dirtB2
-            when 'e'
-              @line << @dirtT1
-            when 'f'
-              @line << @dirtT2
-            when 'g'
-              @line << @dirtL1
-            when 'h'
-              @line << @dirtL2
-            when 'i'
-              @line << @dirtR1
-            when 'j'
-              @line << @dirtR2
-            when 'k'
-              @line << @hwall1
-            when 'l'
-              @line << @hwall2
-            when 'm'
-              @line << @vwall1
-            when 'n'
-              @line << @vwall2
-            when 'o'
-              @line << @BRwallc
-            when 'p'
-              @line << @LBwallc
-            when 'q'
-              @line << @vwallr1
-            when 'r'
-              @line << @vwallr2
-            when 's'
-              @line << @walldt
-            when 't'
-              @line << @walldb
-            when 'u'
-              @line << @walldtr
-            when 'v'
-              @line << @walldbr
-            when 'w'
-              @line << @Thedge1
-            when 'x'
-              @line << @Thedge2
-            when 'y'
-              @line << @Lhedge1
-            when 'z'
-              @line << @Lhedge2
-            when 'A'
-              @line << @Rhedge1
-            when 'B'
-              @line << @Rhedge2
-            when 'C'
-              @line << @Bhedge1
-            when 'D'
-              @line << @Bhedge2
-            when 'E'
-              @line << @BRhedgec
-            when 'F'
-              @line << @LBhedgec
-            when 'G'
-              @line << @TRhedgec
-            when 'H'
-              @line << @LThedgec
-            when 'I'
-              @line << @underbrush1
-            when 'J'
-              @line << @underbrush2
-            when 'K'
-              @line << @tree1
-            when 'L'
-              @line << @tree2
-            when 'M'
-              @line << @tree3
-            when 'N'
-              @line << @tree4
-          end
+    f = File.open("../../src/obj/map/map1_layout.txt")
+    f.each_with_index do |line, y|
+      @line = []
+      line.split(",").each_with_index do |n, x|
+        case n
+          when '1'
+            sprite_to_set = @plain
+          when '2'
+            sprite_to_set = @plain_d
+          when '3'
+            sprite_to_set = @plain_s
+          when '4'
+            sprite_to_set = @stone
+          when '5'
+            sprite_to_set = @flowers
+          when '6'
+            sprite_to_set = @dirt1
+          when '7'
+            sprite_to_set = @dirt2
+          when '8'
+            sprite_to_set = @BRdirtc
+          when '9'
+            sprite_to_set = @LBdirtc
+          when 'a'
+            sprite_to_set = @TRdirtc
+          when 'b'
+            sprite_to_set = @LTdirtc
+          when 'c'
+            sprite_to_set = @dirtB1
+          when 'd'
+            sprite_to_set = @dirtB2
+          when 'e'
+            sprite_to_set = @dirtT1
+          when 'f'
+            sprite_to_set = @dirtT2
+          when 'g'
+            sprite_to_set = @dirtL1
+          when 'h'
+            sprite_to_set = @dirtL2
+          when 'i'
+            sprite_to_set = @dirtR1
+          when 'j'
+            sprite_to_set = @dirtR2
+          when 'k'
+            sprite_to_set = @hwall1
+          when 'l'
+            sprite_to_set = @hwall2
+          when 'm'
+            sprite_to_set = @vwall1
+          when 'n'
+            sprite_to_set = @vwall2
+          when 'o'
+            sprite_to_set = @BRwallc
+          when 'p'
+            sprite_to_set = @LBwallc
+          when 'q'
+            sprite_to_set = @vwallr1
+          when 'r'
+            sprite_to_set = @vwallr2
+          when 's'
+            sprite_to_set = @walldt
+          when 't'
+            sprite_to_set = @walldb
+          when 'u'
+            sprite_to_set = @walldtr
+          when 'v'
+            sprite_to_set = @walldbr
+          when 'w'
+            sprite_to_set = @Thedge1
+          when 'x'
+            sprite_to_set = @Thedge2
+          when 'y'
+            sprite_to_set = @Lhedge1
+          when 'z'
+            sprite_to_set = @Lhedge2
+          when 'A'
+            sprite_to_set = @Rhedge1
+          when 'B'
+            sprite_to_set = @Rhedge2
+          when 'C'
+            sprite_to_set = @Bhedge1
+          when 'D'
+            sprite_to_set = @Bhedge2
+          when 'E'
+            sprite_to_set = @BRhedgec
+          when 'F'
+            sprite_to_set = @LBhedgec
+          when 'G'
+            sprite_to_set = @TRhedgec
+          when 'H'
+            sprite_to_set = @LThedgec
+          when 'I'
+            sprite_to_set = @underbrush1
+          when 'J'
+            sprite_to_set = @underbrush2
+          when 'K'
+            sprite_to_set = @tree1
+          when 'L'
+            sprite_to_set = @tree2
+          when 'M'
+            sprite_to_set = @tree3
+          when 'N'
+            sprite_to_set = @tree4
         end
-        @map_tiles << @line
+        if !sprite_to_set.nil?
+          @line << MapTile.new(sprite_to_set, x*TILE_WIDTH, y*TILE_HEIGHT)
+        end
       end
-  end
-
-  def draw
-    @map_tiles.each_with_index do |arr, ind1|
-      arr.each_with_index do |obj, ind2|
-        obj.img.draw(ind2*12,ind1*12,1)
-      end
+      @map_tiles << @line
     end
   end
 
+  def draw
+    @map_tiles.each do |arr|
+      arr.each do |obj|
+        obj.draw
+      end
+    end
+  end
 
 end
--- src/obj/map/map_tile.rb
+++ src/obj/map/map_tile.rb
@@ -1,7 +1,13 @@
 class MapTile
-  attr_accessor :x,:y,:img
+  attr_accessor :x,:y,:w,:h,:img
 
-  def initialize(sprite)
+  def initialize(sprite, x, y)
     @img = sprite
+    @x = x
+    @y = y
+  end
+
+  def draw
+    @img.draw(@x, @y, 1)
   end
 end
