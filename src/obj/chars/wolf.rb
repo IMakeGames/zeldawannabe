7d3ca2bbc709054c52f41ca49502874d055643dc
IMakeGames <arbolperro@gmail.com>
2018-08-13 08:56:41 -0300

    - Wolf reimplementation Version 2

--- src/obj/chars/wolf.rb
+++ src/obj/chars/wolf.rb
@@ -1,93 +1,85 @@
+# Wolf MOB Class Class.
+# Inherits from Char Class
+# Class Attributes:
+#  *ATTACK_PROBABILITY: Likelyhood that this mob will attack
+#  *sah: Sword attack hitboxes, an array of hitboxes that compose a sword attack.
+#  *una_tiks: Until Next Attack Tiks, counter that determines time before the next attack can be performed.
+#  *unr_tiks: Until Next Roll Tiks, counter that determines time before the next roll can be performed.
+#  *uns_frames: Unitl Next Sheathe Tiks, counter that determines time before next sheath/unsheathe can be performed
+#  *sia: Sword Initial Angle, angle in degrees of the initial sword attack.
+#  *unseathed: boolean attribute that determines whether the main character is in an unsheathed state or not.
+#  *command_stack: Storage for inputed keys and actions.
+#  *changed_command_stack: Indicates whether command_stack has been changed for taking action
+
+
 require '../../src/obj/chars/char'
 require '../anims/wolf_sprite'
 class Wolf < Char
-  ATTACK_PROBABILITY = 50
+  ATTACK_PROBABILITY = 0.5
 
   def initialize(x, y)
     super(x, y, 6, 8, true)
     @face_dir = GameStates::FaceDir::LEFT
     @sprite = WolfSprite.new
     change_state(GameStates::States::IDLE)
-    @attack_x_speed = 0
-    @attack_y_speed = 0
     @attack_dmg = 2
-    @until_next_attack_check = 100
-    @total_hp = 4
-    @current_hp = 4
+    @una_check_tiks = 100                        # Number of tiks after which update will check if attack or not.
+    @total_hp = @current_hp = 4
   end
 
+  # Update behavior is different from Main Char's because it checks on update.
   def update
     if idle?
       if Gosu.distance(@hb.x,@hb.y,$WINDOW.player.hb.x, $WINDOW.player.hb.y) < 150
-        change_state(GameStates::States::MOVING)
+        # If distance is 150 or less, behaviour changes to approaching
+        change_state(GameStates::States::WALKING)
+        @vect_v = 1
+        @vect_angle = Gosu.angle(@hb.x, @hb.y, $WINDOW.player.hb.x, $WINDOW.player.hb.y)
       end
-    elsif moving?
-      move_x = approach($WINDOW.player,1)
-
-      if @until_next_attack_check <= 0
-        dieroll = Random.rand(100)
+    elsif walking?
+      # While walking, wolf tries to approach the main character
+      @vect_angle = Gosu.angle(@hb.x, @hb.y, $WINDOW.player.hb.x, $WINDOW.player.hb.y)
+      if @una_check_tiks <= 0
+        # When next attack check counter reaches 0, wolf can attack.
+        dieroll = Random.rand
         if dieroll <= ATTACK_PROBABILITY
+          # If random is within attack probability, the wolf changes behaviour to attack
           change_state(GameStates::States::ATTACKING)
           @event_tiks =60
+          @vect_v = 0
         else
-          @until_next_attack_check = 30
+          #If random is not within attack probability, next attack check counter is reset to 30.
+          @una_check_tiks = 30
         end
-      else
-        @until_next_attack_check -= 1
       end
-      if move_x < 0
-        change_dir( GameStates::FaceDir::RIGHT)
-      else
-        change_dir( GameStates::FaceDir::LEFT)
-      end
+      check_change_dir
     elsif attacking?
-      perform_attack
-    elsif recoiling?
-      recoil
-    elsif dying?
-      @event_tiks == 0 ? die : @event_tiks -= 1
+      # If attacking, wolf waits for a little while, then charges in a straight line. Then waits a little while.
+      if @event_tiks > 20
+        check_change_dir
+      elsif @event_tiks == 20
+        @vect_angle = Gosu.angle(@hb.x, @hb.y, $WINDOW.player.hb.x, $WINDOW.player.hb.y)
+        @vect_v = 3.5
+      elsif @event_tiks == 10
+        @venct_v = 0
+      elsif @event_tiks <= 0
+        change_state(GameStates::States::IDLE)
+        @una_check_tiks = 100
+      end
     end
 
     if normal? && $WINDOW.player.inv_frames <= 0 && !$WINDOW.player.recoiling? && @hb.check_brute_collision($WINDOW.player.hb)
+      # If it connects with player, player is set to impacted.
       $WINDOW.player.impacted(@hb.center, @attack_dmg)
     end
 
     if !idle? && Gosu.distance(@hb.x,@hb.y,$WINDOW.player.hb.x, $WINDOW.player.hb.y) > 250
+      #If distance surpasses 250, the wolf turns back to idle.
       change_state(GameStates:States::IDLE)
     end
 
-    @can_move_x = true
-    @can_move_y = true
-
     super
-  end
-
-  def perform_attack
-    @event_tiks -= 1
-    if @event_tiks >= 20
-      angle = Gosu.angle($WINDOW.player.hb.x, $WINDOW.player.hb.y, @hb.x, @hb.y)
-      @attack_x_speed = Gosu.offset_x(angle,3.5)
-      @attack_y_speed = Gosu.offset_y(angle,3.5)
-      if @attack_x_speed < 0
-        change_dir( GameStates::FaceDir::RIGHT)
-      else
-        change_dir( GameStates::FaceDir::LEFT)
-      end
-    end
-    if @event_tiks <= 20 && @event_tiks > 0
-    new_hitbox = HitBox.new(@hb.x - @attack_x_speed, @hb.y - @attack_y_speed, @hb.w, @hb.h)
-      $WINDOW.current_map.solid_tiles.each do |tile|
-        if tile.hb.check_brute_collision(new_hitbox)
-          return
-        end
-      end
-      @hb.x -= @attack_x_speed
-      @hb.y -= @attack_y_speed
-    end
-    if @event_tiks <= -15
-      change_state(GameStates::States::IDLE)
-      @until_next_attack_check = 100
-    end
+    @una_check_tiks -= 1 unless idle? || @una_check_tiks <= 0
   end
 
   def change_state(state)
@@ -96,4 +88,14 @@ class Wolf < Char
       @sprite.change_state(GameStates::States::CUSTOM)
     end
   end
+
+  private
+    def check_change_dir
+      # This method changes the direction the wolf, (and therefore it's sprite) is facing in relation to the main char
+      if (@hb.x - $WINDOW.player.hb.x) < 0
+        change_dir( GameStates::FaceDir::RIGHT)
+      else
+        change_dir( GameStates::FaceDir::LEFT)
+      end
+    end
 end
